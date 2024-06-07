17911e643733ca88b3b05da74bf46a472aab44c0
Fabian <eae@carajo.no>
2024-06-07 10:37:08 +0200

    housekeeping

--- assets/javascripts/cookie.js
+++ assets/javascripts/cookie.js
@@ -1,28 +1,26 @@
-(function() {
-	if (typeof cookieNoticeJS === 'undefined') return;
+import "/lib/cookie-notice.js";
 
-	if (location.hostname.match('localhost')) return;
+new cookieNoticeJS({
+	'messageLocales': {
+		'en': "This website uses cookies to provide you with an improved user experience. By continuing to browse this site, you consent to the use of cookies and similar technologies. For further details please visit our"
+	},
+	'buttonLocales': {
+		'en': "OK"
+	},
+	'learnMoreLinkText':{
+		'en': 'privacy policy.'
+	},
+	'learnMoreLinkEnabled': true,
+	'learnMoreLinkHref': '/privacy-policy',
+	'cookieNoticePosition': 'bottom',
+	'expiresIn': 30,
+	'buttonBgColor': '#f0ab00',
+	'buttonTextColor': '#131313',
+	'noticeBgColor': '#000000',
+	'noticeTextColor': '#ffffff',
+	'linkColor': '#e3810a',
+	'linkTarget': '_blank',
+	'debug': false
+});
 
-	new cookieNoticeJS({
-		'messageLocales': {
-			'en': "This website uses cookies to provide you with an improved user experience. By continuing to browse this site, you consent to the use of cookies and similar technologies. For further details please visit our"
-		},
-		'buttonLocales': {
-			'en': "OK"
-		},
-		'learnMoreLinkText':{
-			'en': 'privacy policy.'
-		},
-		'learnMoreLinkEnabled': true,
-		'learnMoreLinkHref': '/privacy-policy',
-		'cookieNoticePosition': 'bottom',
-		'expiresIn': 30,
-		'buttonBgColor': '#f0ab00',
-		'buttonTextColor': '#131313',
-		'noticeBgColor': '#000000',
-		'noticeTextColor': '#ffffff',
-		'linkColor': '#e3810a',
-		'linkTarget': '_blank',
-		'debug': false
-	});
-})();
+if (location.hostname.match('localhost')) document.querySelector('#cookieNotice')?.remove();
